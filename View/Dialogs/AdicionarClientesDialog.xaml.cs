bfd84c57af06c35b381910ca2b505edfba803634
CassianoR <84047010+CassianoR@users.noreply.github.com>
2021-07-04 10:53:46 -0300

    cadastrar cliente por diálogo

---
+++ View/Dialogs/AdicionarClientesDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using LojaOlharDeMenina_WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LojaOlharDeMenina_WPF.View.Dialogs
+{
+    /// <summary>
+    /// Lógica interna para AdicionarClientesDialog.xaml
+    /// </summary>
+    public partial class AdicionarClientesDialog : Window
+    {
+        public AdicionarClientesDialog()
+        {
+            InitializeComponent();
+            DataContext = new ClientesViewModel();
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            this.DragMove();
+        }
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Close();
+        }
+    }
+}
