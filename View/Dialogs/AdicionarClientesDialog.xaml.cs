56e1ccbfeb66b7090c3639d2abb96143b85dbc08
CassianoR <84047010+CassianoR@users.noreply.github.com>
2021-07-04 21:07:05 -0300

    melhorias em UX e no dialog de adicionar clientes

--- View/Dialogs/AdicionarClientesDialog.xaml.cs
+++ View/Dialogs/AdicionarClientesDialog.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using LojaOlharDeMenina_WPF.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace LojaOlharDeMenina_WPF.View.Dialogs
 {
@@ -26,16 +15,23 @@ namespace LojaOlharDeMenina_WPF.View.Dialogs
             DataContext = new ClientesViewModel();
             this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
         }
+
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
 
             this.DragMove();
         }
+
         private void HandleEsc(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Escape)
                 this.Close();
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
