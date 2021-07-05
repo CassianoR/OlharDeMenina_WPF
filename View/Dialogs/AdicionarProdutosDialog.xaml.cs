109d3b8bd5aa1874a0f3f5996cae2a5fb64252a7
CassianoR <84047010+CassianoR@users.noreply.github.com>
2021-07-05 15:16:32 -0300

    adaptações nos dialogs

--- View/Dialogs/AdicionarProdutosDialog.xaml.cs
+++ View/Dialogs/AdicionarProdutosDialog.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using LojaOlharDeMenina_WPF.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace LojaOlharDeMenina_WPF.View.Dialogs
 {
@@ -26,12 +15,14 @@ namespace LojaOlharDeMenina_WPF.View.Dialogs
             DataContext = new ProdutosViewModel();
             this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
         }
+
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
 
             this.DragMove();
         }
+
         private void HandleEsc(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Escape)
@@ -40,7 +31,6 @@ namespace LojaOlharDeMenina_WPF.View.Dialogs
 
         private void btnCadastrar_Click(object sender, RoutedEventArgs e)
         {
-
         }
     }
 }
