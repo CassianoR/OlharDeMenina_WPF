8d997f244ee2e18f1bc9eded91a2971a15f50b4d
Gabriel Fazion <gabriel.fazion@aluno.ifsp.edu.br>
2021-07-05 11:17:42 -0300

    Inserido o código na parte da viewmodel Funcionários e no model1 Funcionários.

--- ViewModel/FuncionariosViewModel.cs
+++ ViewModel/FuncionariosViewModel.cs
@@ -1,6 +1,108 @@
-﻿namespace LojaOlharDeMenina_WPF.ViewModel
+﻿using LojaOlharDeMenina_WPF.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace LojaOlharDeMenina_WPF.ViewModel
 {
-    internal class FuncionariosViewModel
+    public class FuncionariosViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private ObservableCollection<Funcionarios> _lstFuncionarios;
+
+        public ObservableCollection<Funcionarios> lstFuncionarios
+        {
+            get { return _lstFuncionarios; }
+            set
+            {
+                _lstFuncionarios = value;
+                OnPropertyChanged(nameof(lstFuncionarios));
+            }
+        }
+
+        private Funcionarios _selectedFuncionario;
+
+        public Funcionarios SelectedFuncionario
+        {
+            get { return _selectedFuncionario; }
+            set
+            {
+                _selectedFuncionario = value;
+                OnPropertyChanged(nameof(SelectedFuncionario));
+            }
+        }
+
+        private Funcionarios _funcionarios = new Funcionarios();
+
+        public Funcionarios Funcionarios
+        {
+            get { return _funcionarios; }
+            set
+            {
+                _funcionarios = value;
+                OnPropertyChanged(nameof(Funcionarios));
+            }
+        }
+
+        private OlhardeMeninaBDEntities funcionariosEntities;
+
+        public FuncionariosViewModel()
+        {
+            funcionariosEntities = new OlhardeMeninaBDEntities();
+            LoadFuncionario();
+            DeleteCommand = new Command((s) => true, Delete);
+            UpdateCommand = new Command((s) => true, Update);
+            UpdateFuncionarioCommand = new Command((s) => true, UpdateFuncionario);
+            AddFuncionarioCommand = new Command((s) => true, AddFuncionario);
+        }
+
+        private void AddFuncionario(object obj)
+        {
+            Funcionarios.ID = funcionariosEntities.Funcionarios.Count();
+            funcionariosEntities.Funcionarios.Add(Funcionarios);
+            funcionariosEntities.SaveChanges();
+            lstFuncionarios.Add(Funcionarios);
+            Funcionarios = new Funcionarios();
+        }
+
+        private void UpdateFuncionario(object obj) //Update funcionario
+        {
+            funcionariosEntities.SaveChanges();
+            SelectedFuncionario = new Funcionarios();
+        }
+
+        private void Update(object obj) //Update, recarrega
+        {
+            SelectedFuncionario = obj as Funcionarios;
+            LoadFuncionario();
+        }
+
+        private void Delete(object obj) //Delete
+        {
+            var fu = obj as Funcionarios;
+            funcionariosEntities.Funcionarios.Remove(fu);
+            funcionariosEntities.SaveChanges();
+            lstFuncionarios.Remove(fu);
+        }
+
+        private void LoadFuncionario() //Read
+        {
+            lstFuncionarios = new ObservableCollection<Funcionarios>(funcionariosEntities.Funcionarios);
+        }
+
+        public ICommand DeleteCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+        public ICommand UpdateFuncionarioCommand { get; set; }
+        public ICommand AddFuncionarioCommand { get; set; }
     }
+
 }
