43e0fe168998c90a38be37ea2427bc5fe4961404
Ninkinho <kuuhaku.n12@gmail.com>
2021-07-07 19:23:43 -0300

    produtos corrigido

--- Model/Produtos.cs
+++ Model/Produtos.cs
@@ -11,29 +11,111 @@ namespace LojaOlharDeMenina_WPF.Model
 {
     using System;
     using System.Collections.Generic;
-    
-    public partial class Produtos
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public partial class Produtos : INotifyPropertyChanged
     {
+        private int _codigo;
+        private int _fK_CodigoCategoria;
+        private string _nomeProduto;
+        private decimal _valor;
+        private string _descricao;
+        private string _categoria;
+        private string _marca;
+        private string _unidadeMedida;
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Produtos()
         {
             this.Estoque = new HashSet<Estoque>();
             this.Venda = new HashSet<Venda>();
         }
-    
-        public int Codigo { get; set; }
-        public int FK_CodigoCategoria { get; set; }
-        public string NomeProduto { get; set; }
-        public string UnidadeMedida { get; set; }
-        public string Marca { get; set; }
-        public string Categoria { get; set; }
-        public string Descricao { get; set; }
-        public decimal Valor { get; set; }
-    
+
+        public int Codigo
+        {
+            get => _codigo;
+            set
+            {
+                _codigo = value;
+                OnPropertyChanged(nameof(Codigo));
+            }
+        }
+        public int FK_CodigoCategoria
+        {
+            get => _fK_CodigoCategoria;
+            set
+            {
+                _fK_CodigoCategoria = value;
+                OnPropertyChanged(nameof(FK_CodigoCategoria));
+            }
+        }
+        public string NomeProduto
+        {
+            get => _nomeProduto;
+            set
+            {
+                _nomeProduto = value;
+                OnPropertyChanged(nameof(NomeProduto));
+            }
+        }
+        public string UnidadeMedida
+        {
+            get => _unidadeMedida;
+            set
+            {
+                _unidadeMedida = value;
+                OnPropertyChanged(nameof(UnidadeMedida));
+            }
+        }
+        public string Marca
+        {
+            get => _marca;
+            set
+            {
+                _marca = value;
+                OnPropertyChanged(nameof(Marca));
+            }
+        }
+        public string Categoria
+        {
+            get => _categoria;
+            set
+            {
+                _categoria = value;
+                OnPropertyChanged(nameof(Categoria));
+            }
+        }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                _descricao = value;
+                OnPropertyChanged(nameof(Descricao));
+            }
+        }
+        public decimal Valor
+        {
+            get => _valor;
+            set
+            {
+                _valor = value;
+                OnPropertyChanged(nameof(Valor));
+            }
+        }
+
         public virtual Categoria Categoria1 { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Estoque> Estoque { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Venda> Venda { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
