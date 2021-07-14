a9757a231b10df24c8e9d8557d94fd4dc69cd04f
Sarah Siqueira <anonimedsarah@outlook.com>
2021-07-14 13:30:53 -0300

    botão enter no login funciona todos os dialogs tem botão de close agr login tem close

--- View/Login.xaml.cs
+++ View/Login.xaml.cs
@@ -124,5 +124,61 @@ namespace LojaOlharDeMenina_WPF
                 txtNome.Background = new SolidColorBrush(Colors.LavenderBlush);
             }
         }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                lblErro.Content = "";
+                try
+                {
+                    if (txtNome.Text != "" && pb.Password.ToString() != "")
+                    {
+                        SqlCommand objCmd = new SqlCommand("select ID, Cargo, Nome, Senha from funcionarios WHERE Nome = @nome AND Senha = @senha", objCon.Conectar());
+                        objCmd.Parameters.Clear();
+                        objCmd.Parameters.AddWithValue("nome", txtNome.Text);
+                        objCmd.Parameters.AddWithValue("senha", Encrypt(pb.Password.ToString()));
+                        SqlDataReader dr;
+                        dr = objCmd.ExecuteReader();
+                        dr.Read();
+                        if (dr.HasRows)
+                        {
+                            cargo = dr.GetString(1);
+                            if (cargo == "Administrador")
+                            {
+                                f.Adm = true;
+                            }
+                            else
+                            {
+                                f.Adm = false;
+                            }
+                            f.idFunc = dr.GetInt32(0).ToString();
+                            f.username = dr.GetString(2);
+                            f.password = dr.GetString(3);
+                            f.Show();
+                            Close();
+                            objCon.Close();
+                        }
+                        else
+                        {
+                            lblErro.Content = "Usuário ou senha incorretos.";
+                            objCon.Close();
+                        }
+                    }
+                    else
+                    {
+                        lblErro.Content = "Campos de texto não podem estar vazios.";
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Erro de conexão: " + ex);
+                }
+            }
+        }
     }
 }
