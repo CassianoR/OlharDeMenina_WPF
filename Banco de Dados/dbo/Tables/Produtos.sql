5b865e4bc6939b8d681e76a8696f013205b13f60
CassianoR <84047010+CassianoR@users.noreply.github.com>
2021-07-18 14:10:47 -0300

    mudança na categoria -> NomeCategoria como PK

--- Banco de Dados/dbo/Tables/Produtos.sql
+++ Banco de Dados/dbo/Tables/Produtos.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[Produtos] (
     [Codigo]             INT             IDENTITY (1, 1) NOT NULL,
-    [FK_CodigoCategoria] INT             NOT NULL,
+    FK_NomeCategoria     VARCHAR(40)    NOT NULL,
     [NomeProduto]        VARCHAR (50)    NOT NULL,
     [UnidadeMedida]      VARCHAR (15)    NOT NULL,
     [Marca]              VARCHAR (100)   NOT NULL,
     [Descricao]          VARCHAR (100)   NOT NULL,
     [Valor]              DECIMAL (15, 2) NOT NULL,
     PRIMARY KEY CLUSTERED ([Codigo] ASC),
-    CONSTRAINT [fk_produtos] FOREIGN KEY ([FK_CodigoCategoria]) REFERENCES [dbo].[Categoria] ([CodigoCategoria])
+    CONSTRAINT [fk_produtos] FOREIGN KEY ([FK_NomeCategoria]) REFERENCES [dbo].[Categoria] ([NomeCategoria])
 );
 
