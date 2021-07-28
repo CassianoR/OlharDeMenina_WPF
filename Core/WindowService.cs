603016090349d0a82df0d75cc22d60662a43ef68
Gabriel Fazion <gabriel.fazion@aluno.ifsp.edu.br>
2021-07-28 19:08:18 -0300

    Navegação do LoginView para a MainView

---
+++ Core/WindowService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using LojaOlharDeMenina_WPF.View;
+
+namespace LojaOlharDeMenina_WPF.Core
+{
+    interface IWindowService
+    {
+        void showWindow();
+    }
+    
+    class WindowService : IWindowService
+    {
+        public void showWindow()
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+        public void CloseWindow(Window login)
+        {
+            login.Close();
+        }
+    }
+}
