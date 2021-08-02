d41279bae1d5a13b1404d7ebbc13be582b63fe36
Gabriel Fazion <gabriel.fazion@aluno.ifsp.edu.br>
2021-08-02 17:53:59 -0300

    Criadas as modals de Editar Clientes e Funcionários

---
+++ View/Modals/ModalEditarFuncionarios.xaml.cs
@@ -0,0 +1,39 @@
+﻿using LojaOlharDeMenina_WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LojaOlharDeMenina_WPF.View.Modals
+{
+    /// <summary>
+    /// Interação lógica para ModalEditarFuncionarios.xam
+    /// </summary>
+    public partial class ModalEditarFuncionarios : UserControl
+    {
+        public ModalEditarFuncionarios()
+        {
+            InitializeComponent();
+            DataContext = new FuncionariosViewModel();
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+    }
+}
