cb10c166b7d916c9cbb809b241d37040aafab082
Nikinho <58296418+Ninkinho@users.noreply.github.com>
2021-08-04 16:16:26 -0300

    Update EditarCommand.cs

--- Core/Funcionarios/EditarCommand.cs
+++ Core/Funcionarios/EditarCommand.cs
@@ -6,19 +6,19 @@ namespace LojaOlharDeMenina_WPF.Core.Funcionarios
     {
         public override bool CanExecute(object parameter)
         {
-            //var viewModel = parameter as FuncionariosViewModel;
-            //return viewModel != null && viewModel.FuncionarioSelecionado != null;
+            var viewModel = parameter as FuncionariosViewModel;
+            return viewModel != null && viewModel.FuncionarioSelecionado != null;
             return true;
         }
 
         public override void Execute(object parameter)
         {
-            //var viewModel = (FuncionariosViewModel)parameter;
-            //var cloneFuncionario = (Model.Funcionarios)viewModel.FuncionarioSelecionado.Clone();
+            var viewModel = (FuncionariosViewModel)parameter;
+            var cloneFuncionario = (Model.Funcionarios)viewModel.FuncionarioSelecionado.Clone();
 
-            //viewModel.FuncionarioSelecionado.Nome = cloneFuncionario.Nome;
-            //viewModel.FuncionarioSelecionado.Endereco = cloneFuncionario.Endereco;
-            //viewModel.FuncionarioSelecionado.Telefone = cloneFuncionario.Telefone;
+            viewModel.FuncionarioSelecionado.Nome = cloneFuncionario.Nome;
+            viewModel.FuncionarioSelecionado.Endereco = cloneFuncionario.Endereco;
+            viewModel.FuncionarioSelecionado.Telefone = cloneFuncionario.Telefone;
         }
     }
 }
